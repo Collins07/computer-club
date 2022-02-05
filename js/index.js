bef2f42aa0f708955492a9ce4d2fae070aae2e21
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-02-05 15:30:09 +0300

    feat:keyed in variables and values in JS

--- js/index.js
+++ js/index.js
@@ -1,10 +1,102 @@
-function select(link) {
-    const item = link.parentNode
-    const tabs = item.parentNode
-    const index = Array.prototype.indexOf.call(tabs.children,item)
-    const items = tabs.querySelectorAll('tab-item')
-
-    tabs.style.setProperty('--index', index + 1)
-    items.forEach(item => item.classlist.remove('active'))
-    item.classslist.add('active')
-}
+const quiz = [
+    {
+        "question": "What type of language is Javascript?",
+        "answers": {
+            "a":"Object-oriented",
+            "b":"Object-based",
+            "c":"Procedural",
+            "d":"None of the above",
+            "correct":"a"
+        }
+    },
+    {
+        "question": "Which of the following keywords are used to define a varibale in Javascript?",
+        "answers": {
+            "a":"var",
+            "b":"let",
+            "c":"Both of the above",
+            "d":"None of the above",
+            "correct":"c"
+        }
+    },
+    {
+        "question": "Which of the following methods is used to access HTML elements using Javascript?",
+        "answers": {
+            "a":"getElementbyId()",
+            "b":"getElementsByClassName()",
+            "c":"Both A and B",
+            "d":"None of the above",
+            "correct":"c"
+        }
+    },
+    {
+        "question": "Upon encountering empty statements, what does the Javascript interpreter do?",
+        "answers": {
+            "a":"Throws an error",
+            "b":"Ignores the statement",
+            "c":"Gives a warning",
+            "d":"None of the above",
+            "correct":"b"
+        }
+    },
+    {
+        "question": "Which of the following methods can be used to display data in some form using Javascript?",
+        "answers": {
+            "a":"document.write()",
+            "b":"console.log()",
+            "c":"window.alert()",
+            "d":"All of the above",
+            "correct":"d"
+        }
+    },
+    {
+        "question": "What keyword is used to check whether a given property is valid or not?",
+        "answers": {
+            "a":"in",
+            "b":"is in",
+            "c":"exists",
+            "d":"lies",
+            "correct":"a"
+        }
+    },
+    {
+        "question": "When an operator's value is NULL, the typeof returned by the unary operator is?",
+        "answers": {
+            "a":"Boolean",
+            "b":"Undefined",
+            "c":"Object",
+            "d":"Integer",
+            "correct":"c"
+        }
+    },
+    {
+        "question": "What does the 'toLocateString()' method do in JS?",
+        "answers": {
+            "a":"Returns a localised object representation",
+            "b":"Returns a parsed string",
+            "c":"Returns a localised string representation of an object",
+            "d":"None of the above",
+            "correct":"c"
+        }
+    },
+    {
+        "question": "Which function is used to serialize an object into a JSON string in Javascript?",
+        "answers": {
+            "a":"stringify()",
+            "b":"parse()",
+            "c":"convert()",
+            "d":"None of the above",
+            "correct":"a"
+        }
+    },
+    {
+        "question": "Which of the following is not  Javascript framework?",
+        "answers": {
+            "a":"Node",
+            "b":"Vue",
+            "c":"React",
+            "d":"Cassandra",
+            "correct":"d"
+        }
+    },
+]
